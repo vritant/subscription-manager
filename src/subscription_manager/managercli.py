2d125e763b35b19d261346b81a1ab642c98a344c
Adrian Likins <alikins@redhat.com>
2015-04-22 17:44:31 -0400

    Make VersionsCommand use get_client_versions()
    Instead of expecting CliCommand base class to
    have it.

--- src/subscription_manager/managercli.py
+++ src/subscription_manager/managercli.py
@@ -52,7 +52,7 @@ from subscription_manager.release import ReleaseBackend
 from subscription_manager.repolib import RepoActionInvoker, RepoFile
 from subscription_manager.utils import parse_server_info, \
         parse_baseurl_info, format_baseurl, is_valid_server_info, \
-        MissingCaCertException, get_server_versions, \
+        MissingCaCertException, get_client_versions, get_server_versions, \
         restart_virt_who, get_terminal_width, \
         ProductCertificateFilter, EntitlementCertificateFilter
 from subscription_manager.overrides import Overrides, Override
@@ -2539,8 +2539,10 @@ class VersionCommand(CliCommand):
         print (_("server type: %s") % self.server_versions["server-type"])
         print (_("subscription management server: %s") % self.server_versions["candlepin"])
         print (_("subscription management rules: %s") % self.server_versions["rules-version"])
-        print ("subscription-manager: %s" % self.client_versions["subscription-manager"])
-        print ("python-rhsm: %s" % self.client_versions["python-rhsm"])
+
+        client_versions = get_client_versions()
+        print ("subscription-manager: %s" % client_versions["subscription-manager"])
+        print ("python-rhsm: %s" % client_versions["python-rhsm"])
 
 
 class StatusCommand(CliCommand):
